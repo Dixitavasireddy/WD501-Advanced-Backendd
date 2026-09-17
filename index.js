6b81051a5f5bb046a65efa6692ba33a7d801aa26
Dixita vasireddy <dixitavasireddy08@gmail.com>
2026-09-17 19:20:04 +0530

    feat: complete Milestone 3 - implement Todo List with closures

--- index.js
+++ index.js
@@ -1,37 +1,126 @@
-const http = require("http");
-const fs = require("fs");
+const todoList = () => {
+  const all = [];
 
-const port = process.argv[2]
-  ? Number(process.argv[2].split("=")[1])
-  : 5000;
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
 
-const server = http.createServer((req, res) => {
-  let file;
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
 
-  if (req.url === "/" || req.url === "/home") {
-    file = "home.html";
-  } else if (req.url === "/project") {
-    file = "project.html";
-  } else if (req.url === "/registration") {
-    file = "registration.html";
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Page Not Found");
-    return;
-  }
+  const overdue = () => {
+    const today = new Date().toISOString().split("T")[0];
 
-  fs.readFile(file, (error, data) => {
-    if (error) {
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Server Error");
-      return;
-    }
+    return all.filter((item) => item.dueDate < today);
+  };
 
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
-  });
+  const dueToday = () => {
+    const today = new Date().toISOString().split("T")[0];
+
+    return all.filter((item) => item.dueDate === today);
+  };
+
+  const dueLater = () => {
+    const today = new Date().toISOString().split("T")[0];
+
+    return all.filter((item) => item.dueDate > today);
+  };
+
+  const toDisplayableList = (list) => {
+    return list
+      .map((item) => {
+        const checkbox = item.completed ? "[x]" : "[ ]";
+
+        if (item.dueDate === new Date().toISOString().split("T")[0]) {
+          return `${checkbox} ${item.title}`;
+        }
+
+        return `${checkbox} ${item.title} ${item.dueDate}`;
+      })
+      .join("\n");
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+
+// DO NOT CHANGE ANYTHING BELOW THIS LINE.
+
+const todos = todoList();
+
+const formattedDate = (d) => {
+  return d.toISOString().split("T")[0];
+};
+
+const dateToday = new Date();
+
+const today = formattedDate(dateToday);
+
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1)),
+);
+
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1)),
+);
+
+todos.add({
+  title: "Submit assignment",
+  dueDate: yesterday,
+  completed: false,
 });
 
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+todos.add({
+  title: "Pay rent",
+  dueDate: today,
+  completed: true,
 });
+
+todos.add({
+  title: "Service Vehicle",
+  dueDate: today,
+  completed: false,
+});
+
+todos.add({
+  title: "Pay electric bill",
+  dueDate: tomorrow,
+  completed: false,
+});
+
+console.log("My Todo-list\n\n");
+
+console.log("Overdue");
+
+const overdues = todos.overdue();
+const formattedOverdues = todos.toDisplayableList(overdues);
+
+console.log(formattedOverdues);
+
+console.log("\n\n");
+
+console.log("Due Today");
+
+const itemsDueToday = todos.dueToday();
+const formattedItemsDueToday = todos.toDisplayableList(itemsDueToday);
+
+console.log(formattedItemsDueToday);
+
+console.log("\n\n");
+
+console.log("Due Later");
+
+const itemsDueLater = todos.dueLater();
+const formattedItemsDueLater = todos.toDisplayableList(itemsDueLater);
+
+console.log(formattedItemsDueLater);
+
+console.log("\n\n");
